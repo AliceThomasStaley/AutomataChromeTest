3275100152f7d03a8dc9bbf6791c544a8cd4a495
AliceThomasStaley <94575119+AliceThomasStaley@users.noreply.github.com>
2021-12-01 12:02:45 -0600

    version inicial

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using NavegacionAutomatica.Clases;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace NavegacionAutomatica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver(Constantes.StrRutaChromeDriver);
+            driver.Url = "https://www.amazon.com.mx/";
+            IWebElement txtbusqueda = driver.FindElement(By.Id("twotabsearchtextbox"));
+            IWebElement btnBusqueda = driver.FindElement(By.Id("nav-search-submit-button"));
+            txtbusqueda.SendKeys("Nintendo Switch Oled");
+            btnBusqueda.Click();
+
+
+        }
+    }
+}
